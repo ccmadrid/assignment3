e0caf7f1cfe485773df537a7fbd73b4f0fa08a07
ccmadrid <kidmadrid@Carls-MacBook-Pro.local>
2014-04-22 03:19:53 -0400

    copy

---
+++ js/toggle.js
@@ -0,0 +1,23 @@
+var navTop = document.querySelector('.nav-top');
+var logo = document.querySelector('.logo');
+
+document.querySelector('.nav-btn').addEventListener('click', function (e) {
+
+  e.preventDefault();
+
+  if (navTop.getAttribute('data-state'),logo.getAttribute('data-state') == 'expanded') {
+    navTop.setAttribute('data-state', 'collapsed');
+    this.setAttribute('data-state', 'inactive');
+  } else {
+    navTop.setAttribute('data-state', 'expanded');
+    this.setAttribute('data-state', 'active');
+  }
+    
+  if (logo.getAttribute('data-state') == 'expanded') {
+    logo.setAttribute('data-state', 'collapsed');
+    this.setAttribute('data-state', 'inactive');
+  } else {
+    logo.setAttribute('data-state', 'expanded');
+    this.setAttribute('data-state', 'active');
+  }
+});
